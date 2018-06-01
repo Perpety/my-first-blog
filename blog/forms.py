dc8ac99d93019e66e30203d1b888442ffb35aa6c
Perpety <berniceanne3@gmail.com>
2018-06-01 16:13:18 +0000

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django import forms
 
 from .models import Post
@@ -8,15 +7,3 @@ class PostForm(forms.ModelForm):
 	class Meta:
 		model = Post
 		fields = ('title', 'text',)
-=======
-from django import forms
-
-from .models import Post
-
-class PostForm(forms.ModelForm):
-
-	class Meta:
-		model = Post
-		fields = ('title', 'text',)
-		
->>>>>>> a956e3f5d8707afb2fb7939aa59f402ad8377fcd
